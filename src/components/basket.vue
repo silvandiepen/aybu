c226018c143bf3fe1777352ffa0f00b62e9c808a
Sil van Diepen <me@silvandiepen.com>
2020-01-20 11:24:58 +0100

    fixed warnings

--- src/components/basket.vue
+++ src/components/basket.vue
@@ -1,93 +1,93 @@
 <template>
-	<div class="basket">
-		<div class="basket__trigger" @click="active = !active">
-			<div
-				class="basket__amount"
-				:class="{ 'basket__amount--active': basketAmount > 0 }"
-			>
-				{{ basketAmount }}
-			</div>
-			&#10033;
-		</div>
-		<div class="basket__panel" :class="{ 'basket__panel--active': active }">
-			<ul class="basket__list">
-				<li class="basket__item" v-for="(char, idx) in basket" :key="idx">
-					{{ char }}
-				</li>
-			</ul>
-		</div>
-	</div>
+  <div class="basket">
+    <div class="basket__trigger" @click="active = !active">
+      <div
+        class="basket__amount"
+        :class="{ 'basket__amount--active': basketAmount > 0 }"
+      >
+        {{ basketAmount }}
+      </div>
+      &#10033;
+    </div>
+    <div class="basket__panel" :class="{ 'basket__panel--active': active }">
+      <ul class="basket__list">
+        <li class="basket__item" v-for="(char, idx) in basket" :key="idx">
+          {{ char }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	data: () => ({
-		active: false
-	}),
-	computed: {
-		basket() {
-			return this.$store.state.basket.basket;
-		},
-		basketAmount() {
-			return this.$store.getters["basket/amount"];
-		}
-	}
+  data: () => ({
+    active: false
+  }),
+  computed: {
+    basket() {
+      return this.$store.state.basket.basket;
+    },
+    basketAmount() {
+      return this.$store.getters["basket/amount"];
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 .basket {
-	position: fixed;
-	top: grid(1);
-	right: grid(1);
-	&__amount {
-		position: absolute;
-		transform: translate(50%, -50%) scale(0);
-		@include block(Red);
-		@include blockShadow(Red);
-		color: white;
-		padding: 0.25rem;
-		min-width: 1.5rem;
-		font-size: 1rem;
-		height: 1.5rem;
-		font-weight: bold;
-		text-align: center;
-		right: 0;
-		top: 0;
-		text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.25);
-		&--active {
-			transform: translate(50%, -50%) scale(1);
-		}
-	}
-	&__trigger {
-		@include block(IceLight);
-		position: relative;
-		padding: 0.5em;
-		text-align: center;
-		width: auto;
-		display: block;
-		font-size: 1.5em;
-		z-index: 2;
-	}
-	&__panel {
-		@include block(Yellow);
-		position: absolute;
-		top: 1em;
-		right: 1em;
-		transform: scale(0);
-		transition: transform $base-transition $base-cubic-bezier;
-		transform-origin: 100% 0;
-		&--active {
-			transform: scale(1);
-		}
-	}
-	&__list {
-		display: flex;
-	}
-	&__item {
-		width: 2rem;
-		height: 2rem;
-		line-height: 2rem;
-	}
+  position: fixed;
+  top: grid(1);
+  right: grid(1);
+  &__amount {
+    position: absolute;
+    top: 0;
+    right: 0;
+    min-width: 1.5rem;
+    height: 1.5rem;
+    color: white;
+    font-weight: bold;
+    font-size: 1rem;
+    text-align: center;
+    text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.25);
+    transform: translate(50%, -50%) scale(0);
+    padding: 0.25rem;
+    @include block(Red);
+    @include blockShadow(Red);
+    &--active {
+      transform: translate(50%, -50%) scale(1);
+    }
+  }
+  &__trigger {
+    @include block(IceLight);
+    position: relative;
+    z-index: 2;
+    display: block;
+    width: auto;
+    font-size: 1.5em;
+    text-align: center;
+    padding: 0.5em;
+  }
+  &__panel {
+    @include block(Yellow);
+    position: absolute;
+    top: 1em;
+    right: 1em;
+    transform: scale(0);
+    transform-origin: 100% 0;
+    transition: transform $base-transition $base-cubic-bezier;
+    &--active {
+      transform: scale(1);
+    }
+  }
+  &__list {
+    display: flex;
+  }
+  &__item {
+    width: 2rem;
+    height: 2rem;
+    line-height: 2rem;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,46 @@
 <template>
-	<div class="page home">
-		<Basket></Basket>
-		<ul class="character-nav">
-			<li>
-				<button
-					class="button"
-					@click="currentCharset = null"
-					:class="[currentCharset == null ? 'button--yellow' : 'button--red']"
-				>
-					All
-				</button>
-			</li>
-			<li v-for="(charset, idx) in chars" :key="idx">
-				<button
-					class="button"
-					@click="currentCharset = charset.title"
-					:class="[
-						charset.title == currentCharset ? 'button--yellow' : 'button--red'
-					]"
-				>
-					{{ charset.title }}
-				</button>
-			</li>
-		</ul>
-		<section
-			class="character__group"
-			v-for="(charset, idx) in filteredChars"
-			:key="idx"
-		>
-			<h3 class="character__title">{{ charset.title }}</h3>
-			<ul class="character__list">
-				<li
-					class="character__item"
-					v-for="(char, idy) in charset.characters"
-					:key="idy"
-					@click="addToBasket(char)"
-				>
-					{{ char }}
-				</li>
-			</ul>
-		</section>
-	</div>
+  <div class="page home">
+    <Basket></Basket>
+    <ul class="character-nav">
+      <li>
+        <button
+          class="button"
+          @click="currentCharset = null"
+          :class="[currentCharset == null ? 'button--yellow' : 'button--red']"
+        >
+          All
+        </button>
+      </li>
+      <li v-for="(charset, idx) in chars" :key="idx">
+        <button
+          class="button"
+          @click="currentCharset = charset.title"
+          :class="[
+            charset.title == currentCharset ? 'button--yellow' : 'button--red'
+          ]"
+        >
+          {{ charset.title }}
+        </button>
+      </li>
+    </ul>
+    <section
+      class="character__group"
+      v-for="(charset, idx) in filteredChars"
+      :key="idx"
+    >
+      <h3 class="character__title">{{ charset.title }}</h3>
+      <ul class="character__list">
+        <li
+          class="character__item"
+          v-for="(char, idy) in charset.characters"
+          :key="idy"
+          @click="addToBasket(char)"
+        >
+          {{ char }}
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -48,87 +48,87 @@
 import CharacterData from "@/assets/data/characters.json";
 
 Object.filter = (obj, predicate) =>
-	Object.keys(obj)
-		.filter((key) => predicate(obj[key]))
-		.reduce((res, key) => ((res[key] = obj[key]), res), {});
+  Object.keys(obj)
+    .filter(key => predicate(obj[key]))
+    .reduce((res, key) => ((res[key] = obj[key]), res), {});
 
 export default {
-	name: "home",
-	data: () => ({
-		currentCharset: null
-	}),
-	components: {
-		Basket: () => import("@/components/basket.vue")
-	},
-	computed: {
-		chars() {
-			let obj = CharacterData.chars;
-			Object.keys(obj).map((key) => {
-				let str = obj[key];
-				obj[key] = {};
-				obj[key].title = key;
-				obj[key].characters = str.split("");
-			});
-			return obj;
-		},
-		filteredChars() {
-			if (this.currentCharset)
-				return Object.filter(
-					this.chars,
-					(set) => set.title == this.currentCharset
-				);
-			else return this.chars;
-		}
-	},
-	mounted() {
-		console.log(this.chars);
-	},
-	methods: {
-		addToBasket(val) {
-			this.$store.dispatch("basket/addToBasket", val);
-		}
-	}
+  name: "home",
+  data: () => ({
+    currentCharset: null
+  }),
+  components: {
+    Basket: () => import("@/components/basket.vue")
+  },
+  computed: {
+    chars() {
+      let obj = CharacterData.chars;
+      Object.keys(obj).map(key => {
+        let str = obj[key];
+        obj[key] = {};
+        obj[key].title = key;
+        obj[key].characters = str.split("");
+      });
+      return obj;
+    },
+    filteredChars() {
+      if (this.currentCharset)
+        return Object.filter(
+          this.chars,
+          set => set.title == this.currentCharset
+        );
+      else return this.chars;
+    }
+  },
+  mounted() {
+    console.log(this.chars);
+  },
+  methods: {
+    addToBasket(val) {
+      this.$store.dispatch("basket/addToBasket", val);
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 .character {
-	&__group {
-		@include block();
-		& + & {
-			margin-top: 1rem;
-		}
-	}
-	&__title {
-		color: color(Skyblue);
-	}
-	&__list {
-		display: flex;
-		flex-wrap: wrap;
-		align-items: center;
-		justify-content: center;
-	}
-	&__item {
-		display: block;
-		border-radius: $base-border-radius;
-		background-color: color(Turquoise, 0);
-		width: grid(2);
-		height: grid(2);
-		font-size: 1.5rem;
-		line-height: grid(2);
-		text-align: center;
-		&:hover {
-			background-color: color(Turquoise, 1);
-		}
-	}
+  &__group {
+    @include block();
+    & + & {
+      margin-top: 1rem;
+    }
+  }
+  &__title {
+    color: color(Skyblue);
+  }
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  &__item {
+    display: block;
+    width: grid(2);
+    height: grid(2);
+    border-radius: $base-border-radius;
+    background-color: color(Turquoise, 0);
+    font-size: 1.5rem;
+    line-height: grid(2);
+    text-align: center;
+    &:hover {
+      background-color: color(Turquoise, 1);
+    }
+  }
 }
 .character-nav {
-	display: flex;
-	align-items: center;
-	flex-wrap: wrap;
-	justify-content: center;
-	.button {
-		margin: 0.25em;
-	}
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  .button {
+    margin: 0.25em;
+  }
 }
 </style>
