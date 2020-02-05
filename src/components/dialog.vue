148dbfe48835cac0f1d003e5bb2e56f50cba7e80
Sil van Diepen <me@silvandiepen.com>
2020-02-05 10:31:42 +0100

    lintfix

--- src/components/dialog.vue
+++ src/components/dialog.vue
@@ -1,54 +1,54 @@
 <template>
-	<div
-		class="dialog dialog--copy"
-		:class="{ 'dialog--active': show }"
-		@click="show = false"
-	>
-		Copied <span>{{ copied }}</span> to clipboard.
-	</div>
+  <div
+    class="dialog dialog--copy"
+    :class="{ 'dialog--active': show }"
+    @click="show = false"
+  >
+    Copied <span>{{ copied }}</span> to clipboard.
+  </div>
 </template>
 
 <script>
 export default {
-	computed: {
-		copied: {
-			get() {
-				return this.$store.state.dialog.value;
-			}
-		},
-		show: {
-			get() {
-				return this.$store.getters["dialog/GET_ACTIVE"];
-			},
-			set(bool) {
-				this.$store.dispatch("dialog/SET_ACTIVE", {
-					active: bool,
-					value: null
-				});
-			}
-		}
-	}
+  computed: {
+    copied: {
+      get() {
+        return this.$store.state.dialog.value;
+      }
+    },
+    show: {
+      get() {
+        return this.$store.getters["dialog/GET_ACTIVE"];
+      },
+      set(bool) {
+        this.$store.dispatch("dialog/SET_ACTIVE", {
+          active: bool,
+          value: null
+        });
+      }
+    }
+  }
 };
 </script>
 <style lang="scss">
 .dialog {
-	position: fixed;
-	bottom: grid(1);
-	left: 50%;
-	@include block("_Brown");
-	transform: translate(-50%, 0) scale(0);
-	transition: transform $base-transition $base-cubic-bezier;
-	&--active {
-		transform: translate(-50%, -50%) scale(1);
-	}
-	&--copy {
-		span {
-			display: inline-block;
-			padding: 0.5rem;
-			margin: -0.5rem 0.5rem;
-			border-radius: $base-border-radius;
-			background-color: white;
-		}
-	}
+  position: fixed;
+  bottom: grid(1);
+  left: 50%;
+  @include block("_Brown");
+  transform: translate(-50%, 0) scale(0);
+  transition: transform $base-transition $base-cubic-bezier;
+  &--active {
+    transform: translate(-50%, -50%) scale(1);
+  }
+  &--copy {
+    span {
+      display: inline-block;
+      padding: 0.5rem;
+      margin: -0.5rem 0.5rem;
+      border-radius: $base-border-radius;
+      background-color: white;
+    }
+  }
 }
 </style>
